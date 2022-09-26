31485c3dc8b9b6f3181ad9999c57a95a97635b7e
Elizaveta Yastreba <lyizawell@gmail.com>
2022-09-26 20:18:01 +0300

    lab2

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
-
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  var port = 3000
+  app.useStaticAssets(join(__dirname, '..', '/public'));
+  app.setBaseViewsDir(join(__dirname, '../views'));
+  app.set('view engine', 'ejs');
   app.setViewEngine('hbs');
-  const PORT = process.env.PORT || 3000
-  await app.listen(PORT);
+  hbs.registerPartials(join(__dirname, '..', '/views/template'));
+  await app.listen(port);
 }
 bootstrap();
